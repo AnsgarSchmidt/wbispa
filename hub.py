adff2ab9b639b908bb06b58cd231d84887d77ccc
ansi <ansgar.schmidt@gmx.net>
2012-12-28 01:23:37 +0100

    Next Version

--- hub.py
+++ hub.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
 
-import twitter
 import Queue
 import serial
 import threading
-import datetime
+#import datetime
 import time
 import csv
+import eeml
 import os.path
+import ConfigParser
+import _mysql
 
 class ReceiveThread(threading.Thread):
     """Threaded serial receive"""
@@ -46,79 +48,156 @@ class TasksThread(threading.Thread):
 
     def run(self):
         while True:
-            time.sleep(10)
-            self.queue.put("22;") // getTime
-            self.queue.put("23;") // getPWM
-            self.queue.put("26;") // getSolar
-            self.queue.put("27;") // getFlowers
+            time.sleep(5)
+            self.queue.put("22;") # getTemp
+            self.queue.put("23;") # getPWM
+            self.queue.put("26;") # getSolar
+            self.queue.put("27;") # getFlowers
 
-            if !os.path.exists():
+config = ConfigParser.RawConfigParser()
+configModifiedTime = 0
 
-        api = twitter.Api(consumer_key='M6VHvhNpXOSuFHT0dFwQ',
-            consumer_secret='3vji5YgYXDU4sFYAf0kU7HaZwf3cMsXWQPcLYWRAY',
-            access_token_key='995619222-I4dvj9BCTgcvBzvrfANk3ClT1ooqoAkVY0KXysQU',
-            access_token_secret='xYQbcQcovlc16OkrFKK6794guhMfwfEVtMZFAIVA')
+if not os.path.exists('wbispa.cfg'):
+   print "Creating Config File"
+   config.add_section('Hardware')
+   config.set('Hardware', 'Port', '/dev/ttyACM0')
+   config.set('Hardware', 'Speed', '9600')
+   config.set('Hardware', 'MinPWM', '100')
+   config.set('Hardware', 'MinTemp','2300')
+   config.set('Hardware', 'MaxTemp', '2600')
+   config.add_section('CSV')
+   config.set('CSV', 'active', 'false')
+   config.set('CSV', 'filename','wbispa.csv')
+   config.add_section('COSM')
+   config.set('COSM','active', 'false')
+   config.set('COSM','API_KEY','insert key here')
+   config.set('COSM','API_URL','insert number here')
+   config.add_section('MySQL')
+   config.set('MySQL','active','false')
+   config.set('MySQL','Server','127.0.0.1')
+   config.set('MySQL','Database','wbispa')
+   config.set('MySQL','Username','wbispa')
+   config.set('MySQL','Password','blub')
+   with open('wbispa.cfg', 'wb') as configfile:
+    config.write(configfile)
 
-        receiveQueue = Queue.Queue()
-        sendQueue = Queue.Queue()
+config.read('wbispa.cfg')
+configReadTime = 0
 
-        ser = serial.Serial('/dev/ttyUSB0', 9600)
+receiveQueue = Queue.Queue()
+sendQueue = Queue.Queue()
 
-        ftemp = open('temp.csv', 'wt')
-        tempwriter = csv.writer(ftemp)
+ser = serial.Serial(config.get('Hardware','Port'), config.getint('Hardware','Speed'))
 
-        fpwm = open('pwm.csv', 'wt')
-        pwmwriter = csv.writer(fpwm)
+receive = ReceiveThread(receiveQueue, ser)
+receive.setDaemon(True)
+receive.start()
 
-        fsolar = open('solar.csv', 'wt')
-        solarwriter = csv.writer(fsolar)
+send = SendThread(sendQueue, ser)
+send.setDaemon(True)
+send.start()
 
-        fflower = open('flower.csv', 'wt')
-        flowerwriter = csv.writer(fflower)
+tasks = TasksThread(sendQueue)
+tasks.setDaemon(True)
+tasks.start()
 
-        receive = ReceiveThread(receiveQueue, ser)
-        receive.setDaemon(False)
-        receive.start()
+setTempValue = 0
+currentTempValue = 0
+pwmValue = 0
+solarValue = 0
+flower1Value = 0
+flower2Value = 0
 
-        send = SendThread(sendQueue, ser)
-        send.setDaemon(False)
-        send.start()
+setTempUpdated = False
+currentTempUpdated = False
+pwmUpdated = False
+solarUpdated = False
+flower1Updated = False
+flower2Updated = False
 
-        tasks = TasksThread(sendQueue)
-        tasks.setDaemon(False)
-        tasks.start()
-
-        while 1:
-            line = str(receiveQueue.get())
-            element = line.split(',')
-            command = int(element[0])
-            if (command == 1):
+while 1:
+   line = str(receiveQueue.get())
+   element = line.split(',')
+   command = int(element[0])
+   if (command == 1):
+                # Ack
                 print "ACK----->" + line
-            if (command == 2):
-                print "2"
-            if (command == 3):
-                print "3"
-            if (command == 4):
+   if (command == 2):
+                # Ready
+                print "WBISPA ready"
+   if (command == 3):
+                # Error
+                print "ERROR--->" + line
+   if (command == 4):
+                # Request time
                 timestr = "20," + time.strftime("%s", time.localtime()) + ";"
                 sendQueue.put(timestr)
-            if (command == 5):
+   if (command == 5):
+                # Temp
                 num = int(element[1])
-                tempwriter.writerow((time.strftime("%s", time.localtime()), element[2], element[3]))
-                ftemp.flush()
-            if (command == 6):
+                setTempValue = int(element[2])
+                setTempUpdated = True
+                currentTempValue = int(element[3])
+                currentTempUpdated = True
+   if (command == 6):
+                # PWM
                 num = int(element[1])
-                pwmwriter.writerow((time.strftime("%s", time.localtime()), element[2]))
-                fpwm.flush()
-            if (command == 7):
-                print "7"
-            if (command == 8):
+                pwmValue = int (element[2])
+                pwmUpdated = True
+   if (command == 7):
+                # config on arduino board
+                configMaxTemp = int(element[1])
+                configMinTemp = int(element[2])
+                configPwm     = int(element[3])
+                if configMaxTemp != config.getint('Hardware','maxtemp') or configMinTemp != config.getint('Hardware','mintemp') or configPwm != config.getint('Hardware','minpwm'):
+                    configstring = "24,%04d%04d%04d;" % (config.getint('Hardware','maxtemp'),config.getint('Hardware','mintemp'),config.getint('Hardware','minpwm'))
+                    sendQueue.put(configstring)
+   if (command == 8):
+                # time on arduino board
                 print "TIME----->" + line
-            if (command == 9):
-                solarwriter.writerow((time.strftime("%s", time.localtime()), int(element[1])))
-                fsolar.flush()
-            if (command == 10):
-                flowerwriter.writerow((time.strftime("%s", time.localtime()), element[1]))
-                fflower.flush()
+   if (command == 9):
+                # solar value
+                solarValue = int(element[1])
+                solarUpdated = True
+   if (command == 10):
+                # flower level
+                flower1Value = int(element[1])
+                flower1Updated = True
+                flower2Value = int(element[2])
+                flower2Updated = True
 
-            receiveQueue.task_done()
+   receiveQueue.task_done()
 
+   if setTempUpdated == True and currentTempUpdated == True and pwmUpdated == True and solarUpdated == True and flower1Updated == True and flower2Updated == True:
+       setTempUpdated = False
+       currentTempUpdated = False
+       pwmUpdated = False
+       solarUpdated = False
+       flower1Updated = False
+       flower2Updated = False
+       if config.getboolean('CSV','active'):
+           file = open(config.get('CSV','filename'), 'a')
+           csvwriter = csv.writer(file)
+           csvwriter.writerow((time.strftime("%s",time.localtime()),setTempValue,currentTempValue,pwmValue,solarValue,flower1Value,flower2Value))
+           file.flush()
+           file.close()
+       if config.getboolean('MySQL','active'):
+           con = _mysql.connect('127.0.0.1', 'wbispa', 'FuEWtZs8AefmXAXS', 'wbispa')
+           sql = 'INSERT INTO wbispa.measures (setTemp,currentTemp,pwm,flower1,flower2,solar)VALUES(%d,%d,%d,%d,%d,%d);' % (setTempValue,currentTempValue,pwmValue,flower1Value,flower2Value,solarValue)
+           con.query(sql)
+           con.close()
+       if config.getboolean('COSM','active'):
+           pac = eeml.Cosm(config.get('COSM','api_url'), config.get('COSM','api_key'))
+           pac.update([eeml.Data('SetTemperature', setTempValue, unit=eeml.Celsius()),
+                       eeml.Data('CurrentTemperature', currentTempValue, unit=eeml.Celsius()),
+                       eeml.Data('PWM', pwmValue),
+                       eeml.Data('Solar', solarValue),
+                       eeml.Data('FlowerWaterLevel1',flower1Value),
+                       eeml.Data('FlowerWaterLevel2',flower2Value)])
+           pac.put()
+
+   if os.path.getmtime('wbispa.cfg') > configReadTime:
+       print "Config file changed"
+       configReadTime = os.path.getmtime('wbispa.cfg')
+       config.read('wbispa.cfg')
+       sendQueue.put("25;")
