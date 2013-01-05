c652b32f2f45e343a6b8afa68fb2d33c9cf0fb80
ansi <ansgar.schmidt@gmx.net>
2013-01-05 16:52:20 +0100

    Config file as parameter

--- pwmcalc.py
+++ pwmcalc.py
@@ -4,10 +4,16 @@ import csv
 import ConfigParser
 import time
 import os
+import sys
 from twitter import *
 
+CONFIG_FILE = 'wbispa.cfg'
+if len(sys.argv) == 2:
+    CONFIG_FILE = sys.argv[1]
+    print CONFIG_FILE
+
 config = ConfigParser.RawConfigParser()
-config.read('wbispa.cfg')
+config.read(CONFIG_FILE)
 
 MY_TWITTER_CREDS = os.path.expanduser('~/.twitter_oauth')
 CONSUMER_KEY     = config.get('Twitter','consumer_key')
@@ -42,4 +48,4 @@ with open(config.get('CSV','filename'), 'rb') as csvfile:
      hours = str((workingtime - firsttime)/(60*60))
      price = (sumwattperhour / 1000.0) * (float(config.get('PWMCalc','price_per_kwh')))
      string = "In the last " + hours + " hours we used " + str(round(sumwattperhour,3)) + " #watt/hour or " + str(round(price,2)) + " Eurocent for the WBI smart home floor heating system. #hacking #arduino #smarthome"
-     twitter.statuses.update(status=string)
+     #twitter.statuses.update(status=string)
